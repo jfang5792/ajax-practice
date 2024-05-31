7d7486b2b617eca6c4c4025004dbe0ed84fc7322
Jenny Fang <jfang5792@gmail.com>
2024-05-30 17:11:46 -0700

    not running

--- profile.py
+++ profile.py
@@ -4,27 +4,24 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     """Show index page."""
-    
     return render_template("index.html")
-
 
 @app.route('/api/profile', methods=['POST'])
 def profile():
     """Return results from profile form."""
 
-    fullname = request.json['name']
-    # TODO: get the values from the rest of the form
-    # Add them to jsonify
-    
-    return jsonify({'fullname': fullname, })
+    name = request.json['name']
+    age = request.json['age']
+    occupation = request.json['occupation']
 
-
+    return jsonify({'name': name,
+                    'age': age,
+                    'occupation': occupation})
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5200)
